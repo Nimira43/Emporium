3afd9deb616eedfbcf52faefd86f25b7e8676d8f
Nimira43 <n.rabone72@aol.com>
2024-07-09 20:44:41 +0100

    Tested Tailwind - all good

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+// import './index.css'
+
+function App() {
+  return (
+    <div class='text-5xl bg-[#111]  text-[#ff4500] p-5'>
+      <h1 >Emporium</h1>
+      <p className="pt-3">by NimiraTech</p>
+    </div>
+  )
+}
+
+export default App
