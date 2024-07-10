8e19c330728f30ba895d85222352fe37803d00d6
Nimira43 <n.rabone72@aol.com>
2024-07-10 03:06:23 +0100

    Working on Register page. Also created logo for website using Allura font.

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -2,8 +2,14 @@ import React from "react"
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="min-w-screen min-h-screen bg-[#87ceeb] flex justify-center items-center">
+      <dir className="w-[350px] text-[#ff4500] p-2">
+        <div className="bg-[#111] p-4 rounded-md">
+          <h2 className="text-4xl mb-3 font-normal logo">Emporium</h2>
+          <p className="text-sm mb-3 font-light">Register your account</p> 
+        </div>
+      </dir>
+      
     </div>
   )
 }
